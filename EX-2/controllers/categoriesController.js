f4d9727ba313fa5ddba2826edec0c3fdfa6c56d8
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-05-21 18:17:51 +0700

    Update categoriesController.js

--- EX-2/controllers/categoriesController.js
+++ EX-2/controllers/categoriesController.js
@@ -1,44 +1,71 @@
-import { categories, articles } from '../models/data.js';
-
-let categoryList = [...categories];
+import { categories } from '../models/data.js';
 
 export const getAllCategories = (req, res) => {
   res.json(categoryList);
 };
 
 export const getCategoryById = (req, res) => {
-  const category = categoryList.find(c => c.id === parseInt(req.params.id));
-  if (!category) return res.status(404).json({ message: 'Category not found' });
-  res.json(category);
+  const id = req.params.id;
+  const category = categories.find(category => 
+    category.id === parseFloat(id)
+  );
+
+  if (!category) return res.status(404).json({ error: 'Category not found' });
+  return res.status(200).json(category);
 };
 
 export const createCategory = (req, res) => {
+  const { name } = req.body;
+
   const newCategory = {
     id: categoryList.length + 1,
-    ...req.body
+    name
   };
+
   categoryList.push(newCategory);
   res.status(201).json(newCategory);
 };
 
 export const updateCategory = (req, res) => {
-  const index = categoryList.findIndex(c => c.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: 'Category not found' });
+  const id = req.params.id;
+  const { name } = req.body;
 
-  categoryList[index] = { ...categoryList[index], ...req.body };
-  res.json(categoryList[index]);
+  const category = categories.find(category =>
+        category.id === parseInt(id)
+  );
+
+  if (!category) {
+    return res.status(404).json({ error: "Category not found" });
+  }
+
+  category.name = name;
+  return res.status(200).json(category);
 };
 
 export const deleteCategory = (req, res) => {
-  const index = categoryList.findIndex(c => c.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: 'Category not found' });
+  const id = req.params.id;
+  const categoryIndex = categories.findIndex(category =>
+    category.id === parseInt(id)
+  );
 
-  const deleted = categoryList.splice(index, 1);
-  res.json(deleted[0]);
+  if (categoryIndex === -1) {
+    return res.status(404).json({ error: "Category not found" });
+  }
+
+  categories.splice(categoryIndex, 1);
+  return res.status(204).send();
 };
 
 export const getArticlesByCategory = (req, res) => {
-  const id = parseInt(req.params.id);
-  const result = articles.filter(article => article.categoryId === id);
-  res.json(result);
+  const id = req.params.id;
+  const category = categories.find(category =>
+    category.id === parseInt(id)
+  );
+
+  if (!category) {
+    return res.status(404).json({ error: "Category not found" });
+  }
+
+  const articlesByCategory = data.articles.filter(article => article.categoryId === category.id);
+  return res.status(200).json(articlesByCategory);
 };
