2d3c6bf4186c95ba1f4128396ca7590e7e440a7a
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-05-21 18:17:36 +0700

    Update articlesController.js

--- EX-2/controllers/articlesController.js
+++ EX-2/controllers/articlesController.js
@@ -1,38 +1,49 @@
 import { articles } from '../models/data.js';
 
-let articleList = [...articles];
-
 export const getAllArticles = (req, res) => {
-  res.json(articleList);
+  res.json(articles);
 };
 
 export const getArticleById = (req, res) => {
-  const article = articleList.find(a => a.id === parseInt(req.params.id));
-  if (!article) return res.status(404).json({ message: "Article not found" });
+  const article = articles.find(a => a.id === articleId);
+  if (!article) return res.status(404).json({ error: "Article not found" });
   res.json(article);
 };
 
 export const createArticle = (req, res) => {
+  const { title, content, journalistId, categoryId } = req.body;
   const newArticle = {
     id: articleList.length + 1,
-    ...req.body
+    title,
+    content,
+    journalistId,
+    categoryId
   };
+
   articleList.push(newArticle);
   res.status(201).json(newArticle);
 };
 
 export const updateArticle = (req, res) => {
-  const index = articleList.findIndex(a => a.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: "Article not found" });
+  const id = parseInt(req.params.id);
+  const { title, content, journalistId, categoryId } = req.body;
 
-  articleList[index] = { ...articleList[index], ...req.body };
-  res.json(articleList[index]);
+  const article = articles.find(a => a.id === id);
+  if (!article) return res.status(404).json({ error: "Article not found" });
+
+  article.title = title;
+  article.content = content;
+  article.journalistId = journalistId;
+  article.categoryId = categoryId;
+
+  res.json(article);
 };
 
 export const deleteArticle = (req, res) => {
-  const index = articleList.findIndex(a => a.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: "Article not found" });
+  const id = parseInt(req.params.id);
+  const articleIndex = articles.findIndex(a => a.id === id);
+  if (index === -1) return res.status(404).json({ error: "Article not found" });
 
-  const deleted = articleList.splice(index, 1);
-  res.json(deleted[0]);
+  articles.splice(articleIndex, 1);
+  res.json(202).send();
 };
