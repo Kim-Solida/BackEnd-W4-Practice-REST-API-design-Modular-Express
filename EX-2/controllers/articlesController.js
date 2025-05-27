0299c65ba3604dc9151466ca546d4b6b72ce7cb6
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-05-27 17:40:04 +0700

    Update articlesController.js

--- EX-2/controllers/articlesController.js
+++ EX-2/controllers/articlesController.js
@@ -5,6 +5,7 @@ export const getAllArticles = (req, res) => {
 };
 
 export const getArticleById = (req, res) => {
+  const articleId = parseInt(req.params.id);
   const article = articles.find(a => a.id === articleId);
   if (!article) return res.status(404).json({ error: "Article not found" });
   res.json(article);
